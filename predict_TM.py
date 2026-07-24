40b287d75226bcd0648eb026026157ba6fdbda22
kaku85sharma-web <kaku85sharma@gmail.com>
2026-07-24 16:56:53 +0530

    Refactor predict_TM function to return class name

--- predict_TM.py
+++ predict_TM.py
@@ -1,6 +1,6 @@
 def predict_TM(ImageName):
 
- from keras.models import load_model  # TensorFlow is required for Keras to work
+ from tensorflow.keras.models import load_model  # TensorFlow is required for Keras to work
  from PIL import Image, ImageOps  # Install pillow instead of PIL
  import numpy as np
 
