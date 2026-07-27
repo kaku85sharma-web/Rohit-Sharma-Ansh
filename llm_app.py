a2e30cf050e372ee9c3006874c3b4f977269d458
kaku85sharma-web <kaku85sharma@gmail.com>
2026-07-27 15:00:22 +0530

    Update llm_app.py

--- llm_app.py
+++ llm_app.py
@@ -5,7 +5,8 @@ def llm_app(topic):
  from langchain_groq import ChatGroq
  # 1. Initialize your LLM
  load_dotenv()
- groq_api = os.getenv("groq_api_key")
+groq_api_key = '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'
+
  llm = ChatGroq(model='openai/gpt-oss-120b', api_key=groq_api, temperature=0.1)
 
  prompt=PromptTemplate(
